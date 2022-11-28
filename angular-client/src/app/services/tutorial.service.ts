b294a59c2e7e3394faf8d9121aa0268f0816fd5e
NaveedAmanat <44110424+NaveedAmanat@users.noreply.github.com>
2022-11-28 20:50:55 +0500

    Update tutorial.service.ts

--- angular-client/src/app/services/tutorial.service.ts
+++ angular-client/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://10.0.2.15:30000/api/tutorials';
+const baseUrl = 'http://tomcat.example.com/api/tutorials';
 
 //const baseUrl = '/api/tutorials';
 
